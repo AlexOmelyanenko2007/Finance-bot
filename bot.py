d38ff288eeec6d02e1a7466d36bfa8f65a394f8a
Daniil-Fuks <130780639+Daniil-Fuks@users.noreply.github.com>
2023-12-15 08:58:03 +0300

    Создание main.py и bot.py
    В файле main.py запускается сам бот, а в файле bot.py прописывается основная логика бота. Сейчас он на сообщение /start отвечает "Hello".

---
+++ bot.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def start_bot():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(start_bot())
