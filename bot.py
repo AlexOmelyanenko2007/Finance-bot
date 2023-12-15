7795f5efa1087f198af9d550e72989a428f5b4ee
Gritsenko Vladislav <gricenkovlad007@gmail.com>
2023-12-15 13:58:32 +0300

    Приветствие

--- bot.py
+++ bot.py
@@ -10,14 +10,20 @@ bot = Bot(token="")
 # Диспетчер
 dp = Dispatcher()
 
+
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    me = await bot.get_me()
+    await message.answer(f"Привет, {message.from_user.first_name}!👋\n"
+                         f"Я - {me.first_name} для определения выгодности покупки или продажи той"
+                         f" или иной акции, с указанными параметрами!")
+
 
 # Запуск процесса поллинга новых апдейтов
 async def start_bot():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(start_bot())
