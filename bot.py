02b7eeaf44806411182461d9e1f62e9aa8865d0b
Daniil-Fuks <130780639+Daniil-Fuks@users.noreply.github.com>
2023-12-15 15:34:43 +0300

    Добавили кнопку для вызова помощи из меню start
    Теперь при вводе команды /start выводится текст приветствия, а также кнопка, прикрепленная к сообщению. После ее нажатия отсылается текст из пункта "помощь". Возможности вернуться в главное меню нет.

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -15,19 +17,28 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     me = await bot.get_me()
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Помощь 🙋‍",
+        callback_data="help")
+    )
+
     await message.answer(f"Привет, {message.from_user.first_name}!👋\n"
                          f"Я - {me.first_name} для определения выгодности покупки или продажи той"
-                         f" или иной акции, с указанными параметрами!")
+                         f" или иной акции, с указанными параметрами!", reply_markup=builder.as_markup())
 
-@dp.message(Command("help"))
-async def cmd_start(message: types.Message):
-    me = await bot.get_me()
-    await message.answer(f"В мои возможности входит:\n"
-                         f"Проанализировать акции при вводе вручную.")
 
-# Запуск процесса поллинга новых апдейтов
+@dp.callback_query(lambda c: c.data == "help")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer('Торговый бот - это бот, позволяющий'
+                                  ' работать с данными о рынках, которые укажите для анализа с параметрами.'
+                                  ' Благодаря чему бот примет решение по вашему вопросу и выдаст максимально выгодное'
+                                  ' предложение с использованием искусственного интеллекта 🤖.')
+
+
 async def start_bot():
     await dp.start_polling(bot)
+
 
 
 if __name__ == "__main__":
